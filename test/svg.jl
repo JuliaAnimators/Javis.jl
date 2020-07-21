9816ab715bbc72f01a081053bb74132cb7922d4b
Ole Kröger <o.kroeger@opensourc.es>
2020-07-21 13:28:30 +0200

    save not in tmp but remove

--- test/svg.jl
+++ test/svg.jl
@@ -1,9 +1,10 @@
 @testset "O(logn)" begin 
-    d = Drawing(400, 150, "/tmp/test.png")
+    d = Drawing(400, 150, "test.png")
     background("white")
     sethue("black")
     latex(L"\mathcal{O}(\log{n})")
     fillpath()
     finish()
-    @test_reference "refs/ologn.png" load("/tmp/test.png")
+    @test_reference "refs/ologn.png" load("test.png")
+    rm("test.png")
 end
