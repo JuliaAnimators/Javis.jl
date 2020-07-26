a11252319b1aafe22623d2e0bf82e0f0b1539588
Jacob Zelko <jacobszelko@gmail.com>
2020-07-26 16:08:45 -0400

    Failed attempt at using javis for drawing gridlines

--- test/grid_animation.jl
+++ test/grid_animation.jl
@@ -3,34 +3,21 @@ using Luxor
 
 function grid(bg_color="white", hue="black")
 
-	Drawing(500, 500)
-	background(bg_color)
-	sethue(hue)
-
-	A = Point(-250, 250)
-	B = Point(250, 250)
-	line(A, B, :stroke)
-	finish()
+	vid = Video(500, 500)
+	pt = Point(0, 250)
+	end_pt = Point(500, 250)
+	
+	javis(vid, [Action(1:50, ground), 
+		     Action(1:50, :my_line, (args...)->line(pt, end_pt), grow_line(pt)), 
+		     ], tempdirectory="/home/src/Projects/javis/test", creategif=true, pathname="circles.gif")
 end
 
-# function circ(p=O, color="black")
-    # sethue(color)
-    # circle(p, 25, :fill)
-    # return Transformation(p, 0.0)
-# end
+function grow_line(point)
+	point = Point(point.x + 10, point.y)
+	return point
+end
 
-# function anim()
-    # from = Point(-200, -200)
-    # to = Point(-20, -130)
-    # p1 = Point(0,-100)
-    # p2 = Point(0,-50)
-    # from_rot = 0.0
-    # to_rot = 2π
-
-    # demo = Video(500, 500)
-    # javis(demo, [
-        # Action(1:100, ground),
-        # Action(1:100, :red_ball, (args...)->circ(p1, "red"), Rotation(from_rot, to_rot)),
-        # Action(1:100, (args...)->circ(p2, "blue"), Rotation(to_rot, from_rot, :red_ball))
-    # ], tempdirectory="/home/src/Projects/javis/test", creategif=true, pathname="circles.gif")
-# end
+function ground(args...) 
+	background("white")
+	sethue("black")
+end
