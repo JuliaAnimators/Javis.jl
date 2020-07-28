a2865ed0f3c8759f3e2406246f780255a7b5e4ca
Mazen <alotaima@oregonstate.edu>
2020-07-28 12:20:03 -0700

    used Documenter.jl to build documentation page

--- docs/make.jl
+++ docs/make.jl
@@ -6,17 +6,18 @@ makedocs(;
     authors="Ole Kröger <o.kroeger@opensourc.es> and contributors",
     repo="https://github.com/Wikunia/Javis.jl/blob/{commit}{path}#L{line}",
     sitename="Javis.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://Wikunia.github.io/Javis.jl",
-        assets=String[],
-    ),
+    #format=Documenter.HTML(;
+    #    prettyurls=get(ENV, "CI", "false") == "true",
+    #    canonical="https://Wikunia.github.io/Javis.jl",
+    #    assets=String[],
+    #),
     pages=[
         "Home" => "index.md",
         "References" => "references.md",
+        "Contributing" => "contributing.md",
     ],
 )
 
-deploydocs(;
-    repo="github.com/Wikunia/Javis.jl",
-)
+#deploydocs(;
+#    repo="github.com/Wikunia/Javis.jl",
+#)
