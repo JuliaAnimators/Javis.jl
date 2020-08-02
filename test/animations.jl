81fb1b2f2f64472c5f82eec4feb333486a9021e0
Ole Kröger <o.kroeger@opensourc.es>
2020-08-02 19:49:02 +0200

    first version for BackgroundAction (#26)
    * first version for BackgroundAction

--- test/animations.jl
+++ test/animations.jl
@@ -1,6 +1,11 @@
 function ground(args...) 
     background("white")
-    sethue("black")
+    sethue("blue")
+end
+
+function latex_title(args...)
+    translate(0, -200)
+    latex(L"E=mc^2", 20)
 end
 
 function circ(p=O, color="black")
@@ -30,7 +35,33 @@ end
 
     video = Video(500, 500)
     javis(video, [
-        Action(1:25, ground),
+        BackgroundAction(1:25, ground),
+        Action(1:25, latex_title),
+        Action(1:25, :red_ball, (args...)->circ(p1, "red"), Rotation(from_rot, to_rot)),
+        Action(1:25, :blue_ball, (args...)->circ(p2, "blue"), Rotation(to_rot, from_rot, :red_ball)),
+        Action(1:25, (video, args...)->path!(path_of_red, pos(:red_ball), "red")),
+        Action(1:25, (video, args...)->path!(path_of_blue, pos(:blue_ball), "blue")),
+        Action(1:25, (args...)->rad(pos(:red_ball), pos(:blue_ball), "black"))
+    ], tempdirectory="images")
+
+    @test_reference "refs/dancing_circles_16.png" load("images/0000000016.png")
+    for i=1:25
+        rm("images/$(lpad(i, 10, "0")).png")
+    end
+end
+
+@testset "Dancing circles layered" begin 
+    p1 = Point(100,0)
+    p2 = Point(100,80)
+    from_rot = 0.0
+    to_rot = 2π
+    path_of_blue = Point[]
+    path_of_red = Point[]
+
+    video = Video(500, 500)
+    javis(video, [
+        Action(1:25, ground; in_global_layer=true),
+        Action(1:25, latex_title),
         Action(1:25, :red_ball, (args...)->circ(p1, "red"), Rotation(from_rot, to_rot)),
         Action(1:25, :blue_ball, (args...)->circ(p2, "blue"), Rotation(to_rot, from_rot, :red_ball)),
         Action(1:25, (video, args...)->path!(path_of_red, pos(:red_ball), "red")),
