47bf15ee20dce50b94f59fa61661821806a10d6e
Ole Kröger <o.kroeger@opensourc.es>
2020-08-10 18:31:49 +0200

    CodeCov and rename angle to ang (#66)
    * more codecov and renamed angle to ang
    * test for flipped in match_num_point!
    * added some more docs + get_angle and get_position

--- test/animations.jl
+++ test/animations.jl
@@ -14,6 +14,12 @@ function circ(p=O, color="black")
     return p
 end
 
+function circ_ret_trans(p=O, color="black")
+    sethue(color)
+    circle(p, 25, :fill)
+    return Transformation(p, 0.0)
+end
+
 function path!(points, pos, color)
     sethue(color)
     push!(points, pos)
@@ -39,7 +45,7 @@ end
         Action(latex_title),
         Action(Rel(-24:0), :red_ball, (args...)->circ(p1, "red"), Rotation(from_rot, to_rot)),
         Action(1:25, :blue_ball, (args...)->circ(p2, "blue"), Rotation(to_rot, from_rot, :red_ball)),
-        Action(1:25, (video, args...)->path!(path_of_red, pos(:red_ball), "red")),
+        Action(1:25, (video, args...)->path!(path_of_red, get_position(:red_ball), "red")),
         Action(1:25, (video, args...)->path!(path_of_blue, pos(:blue_ball), "blue")),
         Action(1:25, (args...)->rad(pos(:red_ball), pos(:blue_ball), "black"))
     ], tempdirectory="images", pathname="dancing.gif")
@@ -67,6 +73,31 @@ end
         Action(1:25, latex_title),
         Action(1:25, :red_ball, (args...)->circ(p1, "red"), Rotation(to_rot)),
         Action(1:25, :blue_ball, (args...)->circ(p2, "blue"), Rotation(to_rot, from_rot, :red_ball)),
+        Action(1:25, (video, args...)->path!(path_of_red, pos(:red_ball), "red")),
+        Action(1:25, (video, args...)->path!(path_of_blue, pos(:blue_ball), "blue")),
+        Action(1:25, (args...)->rad(pos(:red_ball), pos(:blue_ball), "black"))
+    ], tempdirectory="images", pathname="")
+
+    @test_reference "refs/dancing_circles_16_rot_trans.png" load("images/0000000016.png")
+    for i=1:25
+        rm("images/$(lpad(i, 10, "0")).png")
+    end
+end
+
+@testset "Dancing circles layered return Transformation" begin 
+    p1 = Point(100,0)
+    p2 = Point(100,80)
+    from_rot = 0.0
+    to_rot = 2π
+    path_of_blue = Point[]
+    path_of_red = Point[]
+
+    video = Video(500, 500)
+    javis(video, [
+        Action(1:25, ground, Rotation(π/2, π/2, O), Translation(Point(25,25), Point(25,25)); in_global_layer=true),
+        Action(1:25, latex_title),
+        Action(1:25, :red_ball, (args...)->circ_ret_trans(p1, "red"), Rotation(to_rot)),
+        Action(1:25, :blue_ball, (args...)->circ_ret_trans(p2, "blue"), Rotation(to_rot, from_rot, :red_ball)),
         Action(1:25, (video, args...)->path!(path_of_red, pos(:red_ball), "red")),
         Action(1:25, (video, args...)->path!(path_of_blue, pos(:blue_ball), "blue")),
         Action(1:25, (args...)->rad(pos(:red_ball), pos(:blue_ball), "black"))
@@ -110,18 +141,35 @@ end
     end
 end
 
+@testset "Use returned angle" begin
+    video = Video(500, 500)
+    p = Point(100, 0)
+    javis(video, [
+        Action(1:10, ground),
+        Action(:same, :circ, (args...)->circ_ret_trans(p), Rotation(2π)),
+        Action((args...)->line(Point(-200, 0), Point(-200, -10*ang(:circ)), :stroke))
+    ], tempdirectory="images", pathname="")
+
+    @test_reference "refs/circle_angle.png" load("images/0000000008.png")
+    for i=1:10
+	    rm("images/$(lpad(i, 10, "0")).png")
+    end
+end
+
 astar(args...) = star(Point(-100,-100), 30) 
 acirc(args...) = circle(Point(100,100), 30) 
 
-@testset "morphing star2circle" begin
+@testset "morphing star2circle and back" begin
     video = Video(500, 500)
     javis(video, [
-        Action(1:10, ground),
-        Action(1:10, morph(astar, acirc))
+        Action(1:20, ground),
+        Action(1:10, morph(astar, acirc)),
+        Action(11:20, morph(acirc, astar))
     ], tempdirectory="images", pathname="")
 
     @test_reference "refs/star2circle5.png" load("images/0000000005.png")
-    for i=1:10
+    @test_reference "refs/star2circle15.png" load("images/0000000015.png")
+    for i=1:20
         rm("images/$(lpad(i, 10, "0")).png")
     end
 end
@@ -145,3 +193,4 @@ end
         Action(1:10, morph(astar, acirc))
     ]; pathname="test.mp3") 
 end
+
