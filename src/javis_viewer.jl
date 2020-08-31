b69c94f6e46273c1eccb86919c5e7c11ade6e3c6
Jacob Zelko <jacobszelko@gmail.com>
2020-08-31 13:32:58 -0400

    Proof of concept javis viewer signal emission

--- src/javis_viewer.jl
+++ src/javis_viewer.jl
@@ -1,17 +1,53 @@
 using Gtk
 using GtkReactive
 
-function on_button_clicked(w)
-    println("The Button has been clicked!")
+win = GtkWindow("Javis Viewer", 400, 400)
+
+# Frame to set a specific size on the win.
+fimg = Frame()
+set_gtk_property!(fimg, :height_request, 341) # 341 px according to image size
+set_gtk_property!(fimg, :width_request, 512) # the same
+
+img = Image()
+b1 = Button("Show Image")
+b2 = Button("Clear Image")
+
+input = GtkEntry()
+set_gtk_property!(input, :text, "")
+
+signal_connect(b1, :clicked) do widget
+    set_gtk_property!(img, :file, "/home/src/Projects/javis/src/question_emoji.png")
 end
 
-sl = slider(1:11)
-win = GtkWindow("Javis Viewer", 400, 400)
-bx = GtkBox(:v)
-push!(win, bx)
+signal_connect(b2, :clicked) do widget
+    empty!(img)
+end
 
-push!(bx, sl)
-tb = textbox(Int; signal = signal(sl))
-push!(bx, tb);
+signal_connect(win, "key-press-event") do widget, event
+    mystring = get_gtk_property(tb, :text, String)
+    if event.keyval == 65293
+        println(mystring)
+        # signal_emit(sl, 5)
+    end
+end
+
+g1 = Grid() # Grid to allocate widgets
+set_gtk_property!(g1, :valign, 4) # center all elements in vertical
+set_gtk_property!(g1, :halign, 4) # center all elements in horizontal
+
+# attach image to frame
+push!(fimg, img)
+
+sl = slider(1:11)
+tb = textbox(Int; signal=signal(sl))
+
+# Allocate the widgets in the grid.
+g1[1:2,1] = fimg
+g1[1,2] = b1
+g1[2,2] = b2
+g1[1:2, 3] = sl
+g1[1:2, 4] = tb
+
+push!(win, g1)
 
 Gtk.showall(win)
