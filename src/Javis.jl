c5cdbbf5e911299c41cf9b7e392bceed9f1f8d6d
Jacob Zelko <jacobszelko@gmail.com>
2020-09-14 01:09:55 -0400

    Made short circuit eval of live viewer

--- src/Javis.jl
+++ src/Javis.jl
@@ -1214,6 +1214,11 @@ function javis(
         CURRENT_ACTION[1] = actions[1]
     end
 
+    if liveview == true
+        _javis_viewer(video, length(frames), actions)
+        return "Live preview of $pathname"
+    end
+
     path, ext = "", ""
     if !isempty(pathname)
         path, ext = splitext(pathname)
@@ -1248,28 +1253,23 @@ function javis(
         filecounter += 1
     end
 
-    if liveview == true
-        _javis_viewer(video, length(frames), actions)
-        return "Live preview of $pathname"
+    isempty(pathname) && return
+    if ext == ".gif"
+        # generate a colorpalette first so ffmpeg does not have to guess it
+        ffmpeg_exe(`-loglevel panic -i $(tempdirectory)/%10d.png -vf
+                    "palettegen=stats_mode=diff" -y "$(tempdirectory)/palette.bmp"`)
+        # then apply the palette to get better results
+        ffmpeg_exe(`-loglevel panic -framerate $framerate -i $(tempdirectory)/%10d.png -i
+                    "$(tempdirectory)/palette.bmp" -lavfi
+                    "paletteuse=dither=sierra2_4a" -y $pathname`)
+    elseif ext == ".mp4"
+        finishencode!(video_encoder, video_io)
+        close(video_io)
+        mux("temp.stream", pathname, framerate; silent = true)
     else
-        isempty(pathname) && return
-        if ext == ".gif"
-            # generate a colorpalette first so ffmpeg does not have to guess it
-            ffmpeg_exe(`-loglevel panic -i $(tempdirectory)/%10d.png -vf
-                        "palettegen=stats_mode=diff" -y "$(tempdirectory)/palette.bmp"`)
-            # then apply the palette to get better results
-            ffmpeg_exe(`-loglevel panic -framerate $framerate -i $(tempdirectory)/%10d.png -i
-                        "$(tempdirectory)/palette.bmp" -lavfi
-                        "paletteuse=dither=sierra2_4a" -y $pathname`)
-        elseif ext == ".mp4"
-            finishencode!(video_encoder, video_io)
-            close(video_io)
-            mux("temp.stream", pathname, framerate; silent = true)
-        else
-            @error "Currently, only gif and mp4 creation is supported. Not a $ext."
-        end
-        return pathname
+        @error "Currently, only gif and mp4 creation is supported. Not a $ext."
     end
+    return pathname
 end
 
 """
