668808af4a5b501c3a8da5dd0a71b478d547d749
ARSH SHARMA <184517@nith.ac.in>
2020-10-19 13:18:10 +0530

    Test everything

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,15 +7,15 @@ using ReferenceTests
 using Test
 using VideoIO 
 
-# @testset "Unit" begin
-#     include("unit.jl")
-# end
-# @testset "SVG LaTeX tests" begin
-#     include("svg.jl")
-# end
-# @testset "Animations" begin
-#     include("animations.jl")
-# end
+@testset "Unit" begin
+    include("unit.jl")
+end
+@testset "SVG LaTeX tests" begin
+    include("svg.jl")
+end
+@testset "Animations" begin
+    include("animations.jl")
+end
 @testset "Javis Viewer" begin
     include("viewer.jl")
 end
