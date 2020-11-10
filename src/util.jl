cfac2fdf29ef3161aed787550ca459b4423bf431
Jacob Zelko <jacobszelko@gmail.com>
2020-11-10 12:29:46 -0500

    Clarified warning msg

--- src/util.jl
+++ src/util.jl
@@ -28,10 +28,10 @@ function compute_frames!(
         end
         last_frames = get_frames(elem)
         if !(get_frames(elem) ⊆ available_subframes)
-            @warn("An Action defines frames outside the range of the parental object.
-            In particular Action #$counter for Object #$parent_counter is defined for frames
-            $(get_frames(elem)) but the object exists only for $(length(available_subframes)) frames.
-            (Info: the Background is counted as Object #1)")
+            @warn("Action defined outside the frame range of the parent object.
+            Action #$counter for Object #$parent_counter is defined for frames
+            $(get_frames(elem)) but Object #$parent_counter exists only for $(available_subframes).
+            (Info: Background is counted as Object #1)")
         end
         is_first = false
         counter += 1
