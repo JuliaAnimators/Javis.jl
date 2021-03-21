d0ebbb609a1e6cd8e52d29cfa920b68ae3d3169c
Arsh <184517@nith.ac.in>
2021-03-21 22:23:39 +0530

    Update changes

--- src/Javis.jl
+++ src/Javis.jl
@@ -193,6 +193,7 @@ function render(
     pathname = "javis_$(randstring(7)).gif",
     liveview = false,
     tempdirectory = "",
+    ffmpeg_loglevel = "panic",
 )
     objects = video.objects
     frames = preprocess_frames!(objects)
