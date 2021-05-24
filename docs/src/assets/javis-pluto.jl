a60d6a261235b6eb9cc1bdb1d62ea418a5c0c7d3
Arsh <184517@nith.ac.in>
2021-05-24 14:24:46 +0530

    format

--- docs/src/assets/javis-pluto.jl
+++ docs/src/assets/javis-pluto.jl
@@ -18,43 +18,43 @@ using PlutoUI, Javis
 
 # ╔═╡ ff1ff714-bb9f-11eb-1dfa-fbfa8c75d19f
 begin
-	function ground(args...)
-		background("white") # canvas background
-		sethue("black") # pen color
-	end
+    function ground(args...)
+        background("white") # canvas background
+        sethue("black") # pen color
+    end
 
-	function object(p=O, color="black")
-		sethue(color)
-		circle(p, 25, :fill)
-		return p
-	end
+    function object(p = O, color = "black")
+        sethue(color)
+        circle(p, 25, :fill)
+        return p
+    end
 
-	function path!(points, pos, color)
-		sethue(color)
-		push!(points, pos) # add pos to points
-		circle.(points, 2, :fill) # draws a circle for each point using broadcasting
-	end
+    function path!(points, pos, color)
+        sethue(color)
+        push!(points, pos) # add pos to points
+        circle.(points, 2, :fill) # draws a circle for each point using broadcasting
+    end
 
-	function connector(p1, p2, color)
-		sethue(color)
-		line(p1,p2, :stroke)
-	end
+    function connector(p1, p2, color)
+        sethue(color)
+        line(p1, p2, :stroke)
+    end
 
-	myvideo = Video(500, 500)
+    myvideo = Video(500, 500)
 
-	path_of_red = Point[]
-	path_of_blue = Point[]
+    path_of_red = Point[]
+    path_of_blue = Point[]
 
-	Background(1:70, ground)
-	red_ball = Object(1:70, (args...)->object(O, "red"), Point(100,0))
-	act!(red_ball, Action(anim_rotate_around(2π, O)))
-	blue_ball = Object(1:70, (args...)->object(O, "blue"), Point(200,80))
-	act!(blue_ball, Action(anim_rotate_around(2π, 0.0, red_ball)))
-	Object(1:70, (args...)->connector(pos(red_ball), pos(blue_ball), "black"))
-	Object(1:70, (args...)->path!(path_of_red, pos(red_ball), "red"))
-	Object(1:70, (args...)->path!(path_of_blue, pos(blue_ball), "blue"))
+    Background(1:70, ground)
+    red_ball = Object(1:70, (args...) -> object(O, "red"), Point(100, 0))
+    act!(red_ball, Action(anim_rotate_around(2π, O)))
+    blue_ball = Object(1:70, (args...) -> object(O, "blue"), Point(200, 80))
+    act!(blue_ball, Action(anim_rotate_around(2π, 0.0, red_ball)))
+    Object(1:70, (args...) -> connector(pos(red_ball), pos(blue_ball), "black"))
+    Object(1:70, (args...) -> path!(path_of_red, pos(red_ball), "red"))
+    Object(1:70, (args...) -> path!(path_of_blue, pos(blue_ball), "blue"))
 
-	mygif = render(myvideo; liveview = true)
+    mygif = render(myvideo; liveview = true)
 
 end
 
