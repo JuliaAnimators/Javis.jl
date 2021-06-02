bad6d97d6d1e04350a5fd382c392bc611f09f9bd
Arsh <184517@nith.ac.in>
2021-06-02 17:50:54 +0530

    update test

--- test/viewer.jl
+++ test/viewer.jl
@@ -110,11 +110,11 @@ end
     star_obj = Object(1:100, astar)
     act!(star_obj, Action(morph_to(acirc; do_action = :fill)))
 
-    conf_local = setup_stream(:local)
+    conf_local = setup_stream(:local, address = "0.0.0.0", port=8081)
     @test conf_local isa Javis.StreamConfig
     @test conf_local.livestreamto == :local
     @test conf_local.protocol == "udp"
-    @test conf_local.address == "127.0.0.1"
+    @test conf_local.address == "0.0.0.0"
     @test conf_local.port == 8081
 
     conf_twitch_err = setup_stream(:twitch)
