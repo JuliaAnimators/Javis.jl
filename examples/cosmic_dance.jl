f51deae7abd422db6b0fb21733e8d71ba2504739
Ved Mahajan <vsmahajan2k@gmail.com>
2021-06-24 18:56:31 +0530

    formatted the files

--- examples/cosmic_dance.jl
+++ examples/cosmic_dance.jl
@@ -1,15 +1,15 @@
 using Javis
 
-function object(r,p=O, color="black")
+function object(r, p = O, color = "black")
     sethue(color)
     circle(p, r, :fill)
     return p
 end
 
-function connector!(connection,p1, p2, color)
+function connector!(connection, p1, p2, color)
     sethue(color)
-    push!(connection,[p1,p2])
-    map(x->line(x[1],x[2], :stroke),connection)
+    push!(connection, [p1, p2])
+    map(x -> line(x[1], x[2], :stroke), connection)
 end
 
 function ground(args...)
@@ -32,12 +32,12 @@ function make_animation()
     frames = 1000
 
     # setup the video
-    myvideo = Video(500,500)
-    Background(1:frames,ground)
+    myvideo = Video(500, 500)
+    Background(1:frames, ground)
 
     # add the objects
-    earth = Object(1:frames,(args...)->object(5,O,"blue"),Point(200,0))
-    venus = Object(1:frames,(args...)->object(4,O,"red"),Point(144,0))
+    earth = Object(1:frames, (args...) -> object(5, O, "blue"), Point(200, 0))
+    venus = Object(1:frames, (args...) -> object(4, O, "red"), Point(144, 0))
     #sun = Object(1:frames,(aegs...)->object(50,O,"yellow"),Point(0,0))
 
     # draw the orbits
@@ -45,15 +45,14 @@ function make_animation()
     venus_orbit = Object((args...) -> circ(O, "white", :stroke, 144))
 
     # move the planets
-    act!(earth, Action(anim_rotate_around(12.5*2π, O)))
-    act!(venus, Action(anim_rotate_around(12.5*2π*(224.7/365), O)))
+    act!(earth, Action(anim_rotate_around(12.5 * 2π, O)))
+    act!(venus, Action(anim_rotate_around(12.5 * 2π * (224.7 / 365), O)))
 
     # draw the connectors
-    Object(1:frames, (args...)->connector!(connection,pos(earth), pos(venus), "#f05a4f"))
+    Object(1:frames, (args...) -> connector!(connection, pos(earth), pos(venus), "#f05a4f"))
 
     # render
-    render(myvideo,pathname = "cosmic_dance.gif")
+    render(myvideo, pathname = "cosmic_dance.gif")
 end
 
 make_animation()
-
