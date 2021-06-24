8b8944746fcfc723b1600a90a2af6b1270117b12
Ved Mahajan <vsmahajan2k@gmail.com>
2021-06-24 19:30:18 +0530

    added explaination for ratio

--- examples/cosmic_dance.jl
+++ examples/cosmic_dance.jl
@@ -15,7 +15,6 @@ end
 function ground(args...)
     background("black") # canvas background
     sethue("white") # pen color
-    # circle(O,50,:fill)
 end
 
 function circ(p = O, color = "black", action = :fill, radius = 25, edge = "solid")
@@ -38,13 +37,16 @@ function make_animation()
     # add the objects
     earth = Object(1:frames, (args...) -> object(5, O, "blue"), Point(200, 0))
     venus = Object(1:frames, (args...) -> object(4, O, "red"), Point(144, 0))
-    #sun = Object(1:frames,(aegs...)->object(50,O,"yellow"),Point(0,0))
 
     # draw the orbits
     earth_orbit = Object((args...) -> circ(O, "white", :stroke, 200))
     venus_orbit = Object((args...) -> circ(O, "white", :stroke, 144))
 
     # move the planets
+    # We need the planets to revolve according to their time periods.
+    # Earth completes its one revolution in 365 days and Venus does that in 224.7 days.
+    # Hence, we need to multiply (224.7/365) so that the time period matches properly i.e.,
+    # when earth completes its full revolution, Venus has done (224.7/365) th of its revolution.
     act!(earth, Action(anim_rotate_around(12.5 * 2π, O)))
     act!(venus, Action(anim_rotate_around(12.5 * 2π * (224.7 / 365), O)))
 
