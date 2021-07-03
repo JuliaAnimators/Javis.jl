8389926f2eeb7b3cd9404caabf0c7b6531b92dac
Arsh <184517@nith.ac.in>
2021-07-04 00:56:07 +0530

    JStar

--- src/shorthands.jl
+++ src/shorthands.jl
@@ -85,3 +85,19 @@ JEllipse(focus1::Point, focus2::Point, k::Real; color="black", action=:stroke, s
 Build a polygon approximation to an ellipse, given two points and a point somewhere on the ellipse.
 """
 JEllipse(focus1::Point, focus2::Point, pt::Point; color="black", action=:stroke, stepvalue=pi/100, vertices=false, reversepath=false) = (args...) -> _JEllipse(focus1, focus2, pt, color, action, stepvalue, vertices, reversepath)
+
+#============== Star ==============#
+function _JStar(center, radius, color, npoints, ratio, orientation, action, vertices, reversepath)
+    sethue(color)
+    star(center, radius, npoints, ratio, orientation, action, vertices=vertices, reversepath=reversepath)
+end
+
+"""
+Draw a star centered at a position.
+"""
+JStar(center, radius; color = "black", npoints=5, ratio=0.5, orientation=0, action=:stroke, vertices = false, reversepath=false) = (args...) -> _JStar(center, radius, color, npoints, ratio, orientation, action, vertices, reversepath)
+
+"""
+Make a star. ratio specifies the height of the smaller radius of the star relative to the larger. Use vertices=true to return the vertices of a star instead of drawing it.
+"""
+JStar(xcenter, ycenter, radius; color = "black", npoints=5, ratio=0.5, orientation=0, action=:stroke, vertices = false, reversepath=false) = JStar(Point(xcenter, ycenter), radius, color=color, npoints=npoints, ratio=ratio, orientation=orientation, action=action, vertices=vertices, reversepath=reversepath)
