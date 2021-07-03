a88f686b936e8e68bfef86efc915a87b7f15db11
Arsh <184517@nith.ac.in>
2021-07-04 03:27:47 +0530

    JShape macro JShape

--- src/shorthands/JPoly.jl
+++ src/shorthands/JPoly.jl
@@ -5,8 +5,22 @@ end
 
 """
     JPoly(pointlist::Array{Point, 1}; color="black", action = :stroke, close=true, reversepath=false)
-    
+
 Draw a polygon around points in the pointlist.
 Closes the polygon by default
 """
 JPoly(pointlist::Array{Point, 1}; color="black", action = :stroke, close=true, reversepath=false) = (args...) -> _JPoly(pointlist, color, action, close, reversepath)
+
+
+
+
+function _JShape(body)
+    eval(body)
+end
+
+macro JShape(body::Expr)
+    expr = quote
+        (args...) -> $_JShape($body)
+    end
+    esc(expr)
+end
