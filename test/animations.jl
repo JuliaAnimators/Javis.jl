fa451e468db1fe8571a2cd121119774cb49b8477
Ole Kroeger <o.kroeger@opensourc.es>
2021-07-27 11:09:19 +0200

    fixed test case

--- test/animations.jl
+++ test/animations.jl
@@ -166,7 +166,7 @@ end
     video = Video(500, 500)
     back = Object(1:25, ground, in_global_layer = true)
     act!(back, Action(anim_rotate(π / 2, π / 2)))
-    act!(back, Action(anim_translate(Point(25, 25), Point(25, 25))))
+    act!(back, Action(1:1, anim_translate(Point(25, 25))))
 
     Object(latex_title)
     red_ball = Object(RFrames(-24:0), (args...) -> circ_ret_trans(O, "red"), p1)
