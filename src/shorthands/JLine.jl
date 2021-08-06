cac9b43c223c4fff9d158a48691f49da5457179f
Ole Kroeger <o.kroeger@opensourc.es>
2021-08-06 18:45:29 +0200

    JLine supports linewidth

--- src/shorthands/JLine.jl
+++ src/shorthands/JLine.jl
@@ -1,22 +1,23 @@
-function _JLine(p1, p2, color)
+function _JLine(p1, p2, linewidth, color)
     sethue(color)
+    setline(linewidth)
     line(p1, p2, :stroke)
     return p2
 end
 
 """
-    JLine(p1::Point, p2::Point, color="black")
+    1. JLine(p1::Point, p2::Point; kwargs...)
+    2. JLine(pt2::Point; kwargs...)
+        - `pt1` is set as the origin `O`
 
-Make a line between two points, pt1 and pt2 and do an action.
+# Keywords for all
+- `color` = "black"
+- `linewidth` = 1
+
+Draw a line between the points pt1 and pt2.
 Returns the final point of the line
 """
-JLine(p1::Point, p2::Point; color = "black") =
-    (args...; color = color, p1 = p1, p2 = p2) -> _JLine(p1, p2, color)
+JLine(p1::Point, p2::Point; linewidth = 1, color = "black") =
+    (args...; color = color, linewidth = linewidth, p1 = p1, p2 = p2) -> _JLine(p1, p2, linewidth, color)
 
-"""
-    JLine(pt::Point)
-
-Draw a line from the origin to the pt.
-Returns the final point of the line
-"""
-JLine(pt::Point; color = "black") = JLine(O, pt, color = color)
+JLine(pt::Point; kwargs...) = JLine(O, pt; kwargs...)
