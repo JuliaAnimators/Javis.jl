cb5e34efc7e6fc0331cca78e1e6ff5c58d34139b
Ole Kroeger <o.kroeger@opensourc.es>
2021-08-06 19:52:20 +0200

    JStar without vertices + added linewidth

--- src/shorthands/JStar.jl
+++ src/shorthands/JStar.jl
@@ -2,41 +2,45 @@ function _JStar(
     center,
     radius,
     color,
+    linewidth,
     npoints,
     ratio,
     orientation,
     action,
-    vertices,
     reversepath,
 )
     sethue(color)
-    star(
-        center,
-        radius,
-        npoints,
-        ratio,
-        orientation,
-        action,
-        vertices = vertices,
-        reversepath = reversepath,
-    )
+    setline(linewidth)
+    star(center, radius, npoints, ratio, orientation, action, reversepath = reversepath)
     return center
 end
 
 """
-    JStar(center, radius; color = "black", npoints=5, ratio=0.5, orientation=0, action=:stroke, vertices = false, reversepath=false)
+    1. JStar(center::Point, radius; kwargs...)
+    2. JStar(xcenter, ycenter, radius; kwargs...)
+        - same as 1. with `center = Point(xcenter, ycenter)`
 
-Draw a star centered at a position. Returns the center of the star.
+Draw a star centered at a position. 
+Return the center of the star.
+
+# Keywords for all
+- `color` color of the outline or fill of the star (default: "black")
+- `linewidth` linewidth of the outline (default: 1)
+- `action` defines whether the rectangle should be outlined (`:stroke`) or filled (`:fill`)
+- `npoints` number of points the star has (default: 5)
+- `ratio` height of the smaller radius relative to the larger radius (default: 0.5)
+- `orientation` orientation of the star given by an angle (default: 0)
+- `reversepath` if true it reverses the path and therefore creates a hole (default: true)
 """
 JStar(
-    center,
+    center::Point,
     radius;
     color = "black",
+    linewidth = 1,
     npoints = 5,
     ratio = 0.5,
     orientation = 0,
     action = :stroke,
-    vertices = false,
     reversepath = false,
 ) =
     (
@@ -44,46 +48,20 @@ JStar(
         center = center,
         radius = radius,
         color = color,
+        linewidth = linewidth,
         action = action,
         npoints = npoints,
-        vertices = vertices,
     ) -> _JStar(
         center,
         radius,
         color,
+        linewidth,
         npoints,
         ratio,
         orientation,
         action,
-        vertices,
         reversepath,
     )
 
-"""
-    JStar(xcenter, ycenter, radius; color = "black", npoints=5, ratio=0.5, orientation=0, action=:stroke, vertices = false, reversepath=false)
-
-Make a star. ratio specifies the height of the smaller radius of the star relative to the larger. Use vertices=true to return the vertices of a star instead of drawing it.
-Returns the center of the star.
-"""
-JStar(
-    xcenter,
-    ycenter,
-    radius;
-    color = "black",
-    npoints = 5,
-    ratio = 0.5,
-    orientation = 0,
-    action = :stroke,
-    vertices = false,
-    reversepath = false,
-) = JStar(
-    Point(xcenter, ycenter),
-    radius,
-    color = color,
-    npoints = npoints,
-    ratio = ratio,
-    orientation = orientation,
-    action = action,
-    vertices = vertices,
-    reversepath = reversepath,
-)
+JStar(xcenter, ycenter, radius; kwargs...) =
+    JStar(Point(xcenter, ycenter), radius; kwargs...)
