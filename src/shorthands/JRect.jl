6b420baaa35b63ec11590716c461c66bcb2c10b1
Ole Kroeger <o.kroeger@opensourc.es>
2021-08-06 19:42:31 +0200

    JRect removed vertices, added linewidth, fixed color

--- src/shorthands/JRect.jl
+++ src/shorthands/JRect.jl
@@ -2,42 +2,45 @@ function _JRect(
     cornerpoint::Point,
     w::Real,
     h::Real,
-    color::String,
+    color,
+    linewidth::Real,
     action::Symbol,
-    vertices::Bool,
 )
     sethue(color)
-    rect(cornerpoint, w, h, action; vertices = vertices)
+    setline(linewidth)
+    rect(cornerpoint, w, h, action)
     return cornerpoint
 end
 
 """
-    JRect(cornerpoint::Point, w::Real, h::Real; color="black", action=:stroke, vertices=false)
+    1. JRect(cornerpoint::Point, w::Real, h::Real; kwargs...)
+    2. JRect(xmin::Int64, ymin::Int64, w::Real, h::Real; kwargs...)
+        - same as 1. with `cornerpoint = Point(xmin, ymin)`
 
 Create a rectangle with one corner at cornerpoint with width w and height h and do an action.
+You can specify the `linewidth` and the `color` of the rectangle.
+
+# Keywords for all
+- `color` = "black"
+- `linewidth` = 1
+- `action` Defines whether the rectangle should be outlined (`:stroke`) or filled (`:fill`)
 """
 JRect(
     cornerpoint::Point,
     w::Real,
     h::Real;
     color = "black",
+    linewidth = 1,
     action = :stroke,
-    vertices = false,
 ) =
     (
         args...;
-        kcornerpoint = cornerpoint,
+        cornerpoint = cornerpoint,
         w = w,
         h = h,
         color = color,
+        linewidth = linewidth,
         action = action,
-        vertices = vertices,
-    ) -> _JRect(kcornerpoint, w, h, color, action, vertices)
+    ) -> _JRect(cornerpoint, w, h, color, linewidth, action)
 
-"""
-    JRect(xmin::Int64, ymin::Int64, w::Real, h::Real; color="black", action=:stroke)
-
-Create a rectangle with one corner at (xmin/ymin) with width w and height h and then do an action.
-"""
-JRect(xmin::Int64, ymin::Int64, w::Real, h::Real; color = "black", action = :stroke) =
-    JRect(Point(xmin, ymin), w, h, color = color, action = action)
+JRect(xmin::Int64, ymin::Int64, w::Real, h::Real; kwargs...) = JRect(Point(xmin, ymin), w, h; kwargs...)
