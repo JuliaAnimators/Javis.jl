2d350a0306fe145848818c0efbadf4917d5acc63
Ole Kroeger <o.kroeger@opensourc.es>
2021-08-06 18:38:27 +0200

    JCircle restructure and support for linewidth

--- src/shorthands/JCircle.jl
+++ src/shorthands/JCircle.jl
@@ -1,43 +1,33 @@
-function _JCircle(center, radius, color, action)
+function _JCircle(center, radius, linewidth, color, action)
     sethue(color)
+    setline(linewidth)
     circle(center, radius, action)
     return center
 end
 
 """
-    JCircle(center::Point, radius::Real; color="black", action=:stroke)
+    1. JCircle(center::Point, radius::Real; kwargs...)
+    2. JCircle(center_x::Real, center_y::Real, radius::Real; kwargs...)
+    3. JCircle(p1::Point, p2::Point; kwargs...)
+        - A circle that touches `p1` and `p2`
+    4. JCircle(radius::Real)
+        - A circle at the origin
 
-Make a circle centered at pt.
+# Keywords for all
+- `color` = "black"
+- `linewidth` = 1
+- `action::Symbol` :stroke by default can be ``:fill` or other actions explained in the Luxor documentation.
+
 Returns the center of the circle
 """
-JCircle(center::Point, radius::Real; color = "black", action = :stroke) =
+JCircle(center::Point, radius::Real; color = "black", linewidth=1, action = :stroke) =
     (args...; center = center, radius = radius, color = color, action = action) ->
-        _JCircle(center, radius, color, action)
+        _JCircle(center, radius, linewidth, color, action)
 
-"""
-    JCircle(center_x::Real, center_y::Real, radius::Real; color="black", action=:stroke)
+JCircle(center_x::Real, center_y::Real, radius::Real; kwargs...) =
+    JCircle(Point(center_x, center_y), radius; kwargs...)
 
-Make a circle of radius r centered at x/y.
-action is one of the actions applied by do_action, defaulting to :none. You can also use ellipse() to draw circles and place them by their centerpoint.
-Returns the center of the circle
-"""
-JCircle(center_x::Real, center_y::Real, radius::Real; color = "black", action = :stroke) =
-    JCircle(Point(center_x, center_y), radius, color = color, action = action)
+JCircle(p1::Point, p2::Point; kwargs...) =
+    JCircle(midpoint(p1, p2), distance(p1, p2) / 2; kwargs...)
 
-"""
-    JCircle(p1::Real, p2::Real; color="black", action=:stroke)
-
-Make a circle that passes through two points that define the diameter:
-Returns the center of the circle
-"""
-JCircle(p1::Point, p2::Point; color = "black", action = :stroke) =
-    JCircle(midpoint(p1, p2), distance(p1, p2) / 2, color = color, action = action)
-
-"""
-    JCircle(radius::Real; color="black", action=:stroke)
-
-Make a circle that passes through three points.
-Returns the center of the circle
-"""
-JCircle(radius::Real; color = "black", action = :stroke) =
-    JCircle(O, radius, color = color, action = action)
+JCircle(radius::Real; kwargs...) = JCircle(O, radius; kwargs...)
