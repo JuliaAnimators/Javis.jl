e41c044112f15af221fe92bbc637fb85651f08b9
Ole Kroeger <o.kroeger@opensourc.es>
2021-08-06 19:32:29 +0200

    linewidth for JPoly

--- src/shorthands/JPoly.jl
+++ src/shorthands/JPoly.jl
@@ -1,20 +1,33 @@
-function _JPoly(pointlist, color, action, close, reversepath)
+function _JPoly(pointlist, color, linewidth, action, close, reversepath)
     sethue(color)
+    setline(linewidth)
     poly(pointlist, action; close = close, reversepath = reversepath)
 end
 
 """
-    JPoly(pointlist::Array{Point, 1}; color="black", action = :stroke, close=true, reversepath=false)
+    JPoly(pointlist::Vector{Point}; color="black", linewidth=1, action = :stroke, close=true, reversepath=false)
 
 Draw a polygon around points in the pointlist.
-Closes the polygon by default
+
+# Keywords
+- `color` specifies the color of the outline or the fill of it (depends on action)
+- `linewidth` linewidth of the outline
+- `action` can be `:stroke`, `:fill` or other symbols (check the Luxor documentation for details) (default: :stroke)
+- `close` whether the polygon should be closed or not (default: closed) 
+- `reversepath` can be set to `true` to reverse the path and create a polygon hole
 """
 JPoly(
-    pointlist::Array{Point,1};
+    pointlist::Vector{Point};
     color = "black",
+    linewidth = 1,
     action = :stroke,
     close = true,
     reversepath = false,
 ) =
-    (args...; pointlist = pointlist, color = color, action = action) ->
-        _JPoly(pointlist, color, action, close, reversepath)
+    (
+        args...;
+        pointlist = pointlist,
+        color = color,
+        linewidth = linewidth,
+        action = action,
+    ) -> _JPoly(pointlist, color, linewidth, action, close, reversepath)
