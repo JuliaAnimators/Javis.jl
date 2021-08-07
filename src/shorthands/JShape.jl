2b273a5e190826c634f134131d7d42a65a87f971
Jacob Zelko <jacobszelko@gmail.com>
2021-08-06 22:28:58 -0400

    Formatted docstring some

--- src/shorthands/JShape.jl
+++ src/shorthands/JShape.jl
@@ -1,17 +1,19 @@
 """
     @JShape(body, args...)
 
-Creates a custom shape based on the luxor instructions in the `begin...end` block
+Macro that creates a user-defined shape based on `Luxor` instructions within its `begin...end` block
+
+# Example
+
 ```julia
-    Object(@JShape begin
-        sethue(color)
-        poly(points, action, close= true)
-    end action = :stroke color ="red" radius = 8 
-)
+Object(@JShape begin
+    sethue(color)
+    poly(points, action, close = true)
+end action = :stroke color = "red" radius = 8)
 ```
 
-The arguments after the `end` i.e `action = :stroke color ="red" radius = 8`
-can be used inside the `bgein...end` block and can be animated using the [`change`](@ref) action.
+In this example, the arguments after the `end` (i.e. `action = :stroke color = "red" radius = 8`)
+can be used inside the `bgein...end` block and animated using the [`change`](@ref) action.
 """
 macro JShape(body::Expr, expr...)
     leftside = Expr(
