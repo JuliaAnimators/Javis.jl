e1f5cd8b4c3125ff02fd42d2131696ba65491961
Jacob Zelko <jacobszelko@gmail.com>
2021-08-14 15:25:05 -0400

    Made argument names less confusing

--- src/shorthands/JImage.jl
+++ src/shorthands/JImage.jl
@@ -1,17 +1,17 @@
-function _JImage(pos, img, centering, clipargs, clipshape)
-    if clipshape != false
-        clipshape(clipargs...)
+function _JImage(pos, img, centering, shapeargs, shape)
+    if shape != false
+        shape(shapeargs...)
     end
     placeimage(img, pos, centered = centering)
     return pos
 end
 
-JImage(pos::Point, img, centering = true; clipargs = (), clipshape = false) =
+JImage(pos::Point, img, centering = true; shapeargs = (), shape = false) =
     (
         args...;
         pos = pos,
         img = img,
         centering = centering,
-        clipargs = clipargs,
-        clipshape = clipshape,
-    ) -> _JImage(pos, img, centering, clipargs, clipshape)
+        shapeargs = shapeargs,
+        shape = shape,
+    ) -> _JImage(pos, img, centering, shapeargs, shape)
