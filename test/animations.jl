b173d4213a7c87d1cd6122c13e7cce9ff0b35001
gpucce <g.puccetti92@gmail.com>
2021-09-14 17:59:53 +0200

    add contemporary animation and switch to square so rotate makes sense

--- test/animations.jl
+++ test/animations.jl
@@ -720,42 +720,31 @@ end
     function act_all!(a)
         act!(a, Action(1:70, anim_translate(Point(50, 50))))
         act!(a, Action(71:140, anim_translate(Point(50, 50), Point(-50, -50))))
+        act!(a, Action(71:140, anim_rotate(2π)))
+        act!(a, Action(140:210, anim_translate(Point(50, 50))))
     end
 
     n_frames = 257
     nolayer_video = Video(500, 500)
     Background(1:n_frames, ground)
-    circ = Object(JCircle(O, 20, action = :fill, color = "white"))
+    circ = Object(JBox(O, 20, 20, action = :fill, color = "white"))
     act_all!(circ)
     render(nolayer_video, tempdirectory = "images/without_layer", pathname = "")
 
     layer_video = Video(500, 500)
     Background(1:n_frames, ground)
     l1 = @JLayer 1:n_frames begin
-        Object(JCircle(O, 20, action = :fill, color = "white"))
+        Object(JBox(O, 20, 20, action = :fill, color = "white"))
     end
     act_all!(l1)
     render(layer_video, tempdirectory = "images/with_layer", pathname = "")
 
-    @test_reference "images/with_layer/0000000008.png" load(
-        "images/without_layer/0000000008.png",
-    )
-    @test_reference "images/with_layer/0000000032.png" load(
-        "images/without_layer/0000000032.png",
-    )
-    @test_reference "images/with_layer/0000000128.png" load(
-        "images/without_layer/0000000128.png",
-    )
-
-    @test_reference "images/without_layer/0000000016.png" load(
-        "images/with_layer/0000000016.png",
-    )
-    @test_reference "images/without_layer/0000000064.png" load(
-        "images/with_layer/0000000064.png",
-    )
-    @test_reference "images/without_layer/0000000256.png" load(
-        "images/with_layer/0000000256.png",
-    )
+    for frame in [8, 16, 32, 64, 128, 256]
+        png_name = lpad(string(frame), 10, "0")
+        @test_reference "images/with_layer/$png_name.png" load(
+            "images/without_layer/$png_name.png",
+        )
+    end
 
     for i in 1:n_frames
         rm("images/with_layer/$(lpad(i, 10, "0")).png")
