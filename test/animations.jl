0f31cc946c0f60af2868dfc68cd5ae040a9226ba
gpucce <g.puccetti92@gmail.com>
2021-09-14 18:19:37 +0200

    add shifting test with single shift on layer and double shift on Object

--- test/animations.jl
+++ test/animations.jl
@@ -752,4 +752,36 @@ end
     end
     rm("images/with_layer/", recursive = true)
     rm("images/without_layer/", recursive = true)
+
+    n_frames = 140
+    nolayer_video = Video(500, 500)
+    Background(1:n_frames, ground)
+    circ = Object(JBox(O, 20, 20, action = :fill, color = "white"))
+
+    act!(circ, Action(1:140, anim_translate(Point(0, 50))))
+    act!(circ, Action(1:140, anim_translate(Point(50, 0))))
+
+    render(nolayer_video, tempdirectory = "images/without_layer", pathname = "")
+
+    layer_video = Video(500, 500)
+    Background(1:n_frames, ground)
+    l1 = @JLayer 1:n_frames begin
+        Object(JBox(O, 20, 20, action = :fill, color = "white"))
+    end
+    act!(l1, Action(1:140, anim_translate(Point(50, 50))))
+    render(layer_video, tempdirectory = "images/with_layer", pathname = "")
+
+    for frame in [8, 16, 24, 32, 40, 48]
+        png_name = lpad(string(frame), 10, "0")
+        @test_reference "images/with_layer/$png_name.png" load(
+            "images/without_layer/$png_name.png",
+        )
+    end
+
+    for i in 1:140
+        rm("images/with_layer/$(lpad(i, 10, "0")).png")
+        rm("images/without_layer/$(lpad(i, 10, "0")).png")
+    end
+    rm("images/with_layer/", recursive = true)
+    rm("images/without_layer/", recursive = true)
 end
