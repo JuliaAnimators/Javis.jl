f2ce80d5e867dc0cc095e4a91eaa5273b7acc34e
gpucce <g.puccetti92@gmail.com>
2021-11-04 20:58:54 +0100

    add +,- for betwenn delayedposition and poin

--- src/structs/Delayed.jl
+++ src/structs/Delayed.jl
@@ -1,5 +1,21 @@
 mutable struct DelayedPosition
     obj::AbstractObject
-    position::Union{Nothing,Point}
+    position::Point
     called::Bool
+end
+
+import Base: +, -
+
+function +(dp::DelayedPosition, p::Point)
+    DelayedPosition(dp.obj, dp.position + p, dp.called)
+end
+
++(p::Point, dp::DelayedPosition) = +(dp, p)
+
+function -(dp::DelayedPosition, p::Point)
+    return DelayedPosition(dp.obj, dp.position - p, dp.called)
+end
+
+function -(p::Point, dp::DelayedPosition)
+    return DelayedPosition(dp.obj, p - dp.position, dp.called)
 end
