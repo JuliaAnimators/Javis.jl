84991286e874244a3a7c4777d54f1e4241d1bc8c
gpucce <g.puccetti92@gmail.com>
2021-11-07 10:06:17 +0100

    adjust tests

--- test/delayed.jl
+++ test/delayed.jl
@@ -58,6 +58,33 @@
     mkdir("images/delayed/")
     render(testvideo, tempdirectory = "images/delayed/", pathname = "")
 
+    for frame in [1, 5, 14, 24, 30]
+        png_name = lpad(frame, 10, "0")
+        @test_reference "images/delayed/$(png_name).png" load(
+            "images/not_delayed/$(png_name).png",
+        )
+    end
+
+    rm("images/delayed", recursive = true)
+
+    testvideo = Video(300, 300)
+    Background(1:31, (args...) -> begin
+        background("white")
+        sethue("black")
+    end)
+    ball1 = Object(1:31, JCircle(O, 10, color = "black", action = :fill), Point(-45, 0))
+    ball2 = Object(1:31, JCircle(O, 10, color = "blue", action = :fill), Point(0, 0))
+    act!(ball1, Action(1:31, anim_translate(Point(-45, 0), Point(45, 0))))
+    act!(
+        ball2,
+        Action(
+            16:31,
+            anim_translate(Javis.delayed_pos(ball1) - Point(-25, 0), Point(0, -45)),
+        ),
+    )
+    mkdir("images/delayed/")
+    render(testvideo, tempdirectory = "images/delayed/", pathname = "")
+
 
     for frame in [1, 5, 14, 24, 30]
         png_name = lpad(frame, 10, "0")
