80957f1eb0c1976cc5b74deea21786b26fe7dfcb
Jacob Zelko <jacobszelko@gmail.com>
2021-12-29 20:41:46 -0500

    Fixed docstrings

--- src/shorthands/JImage.jl
+++ src/shorthands/JImage.jl
@@ -8,18 +8,57 @@ function _JImage(pos, img, centering, shapeargs, shape, scaleargs)
 end
 
 """
-    JImage(pos::Point, img, centering = true; shapeargs = (), shape = nothing, scaleargs = 1)
+    JImage(pos::Point, img::CairoSurfaceBase{UInt32}, centering = true; shapeargs = (), shape = nothing, scaleargs = 1)
 
 Place a given image at a given location as a `Javis` object.
 Images can be cropped to different shapes and scaled to different sizes while being placed.
 
-Returns the position of the image location.
+# Arguments
+- `pos::Point`: Where to place the image inside a shape.
+- `img::CairoSurfaceBase{UInt32}`: Expects a CairoSurfaceBase object via `readpng("your_image.png")`
+- `centering::Bool`: Centers the object at `pos`
+- `shapeargs`: Arguments to be passed to a given shape type
+- `shape`: A Luxor shape function such as `circle`, `box`, etc.
+- `scaleargs`: The arguments used for scaling the image used on the shape
+
+# Return
+
+Returns the position of the image location, `pos`.
 """
-JImage(pos::Point, img, centering = true; shapeargs = (), shape = nothing, scaleargs = 1) =
+JImage(pos::Point, img::CairoSurfaceBase{UInt32}, centering::Bool = true; shapeargs = (), shape = nothing, scaleargs = 1) =
     (
         args...;
         pos = pos,
         img = img,
+        centering = centering,
+        shapeargs = shapeargs,
+        shape = shape,
+	scaleargs = scaleargs,
+    ) -> _JImage(pos, img, centering, shapeargs, shape, scaleargs)
+
+"""
+    JImage(pos::Point, img::CairoSurfaceBase{UInt32}, centering = true; shapeargs = (), shape = nothing, scaleargs = 1)
+
+Place a given image at a given location as a `Javis` object.
+Images can be cropped to different shapes and scaled to different sizes while being placed.
+
+# Arguments
+- `pos::Point`: Where to place the image inside a shape
+- `img::String`: Expects the path to an image
+- `centering::Bool`: Centers the object at `pos`
+- `shapeargs`: Arguments to be passed to a given shape type
+- `shape`: A Luxor shape function such as `circle`, `box`, etc.
+- `scaleargs`: The arguments used for scaling the image used on the shape
+
+# Return
+
+Returns the position of the image location, `pos`.
+"""
+JImage(pos::Point, img::String, centering::Bool = true; shapeargs = (), shape = nothing, scaleargs = 1) =
+    (
+        args...;
+        pos = pos,
+        img = readpng(img),
         centering = centering,
         shapeargs = shapeargs,
         shape = shape,
