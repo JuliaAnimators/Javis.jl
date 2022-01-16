92f36cab13d0b6391b331ed2f83b5e61d1a2524e
Jacob Zelko <jacobszelko@gmail.com>
2022-01-16 18:37:59 -0500

    Added image support dispatch for JPoly

--- src/shorthands/JPoly.jl
+++ src/shorthands/JPoly.jl
@@ -13,7 +13,7 @@ Draw a polygon around points in the pointlist.
 - `color` specifies the color of the outline or the fill of it (depends on action)
 - `linewidth` linewidth of the outline
 - `action` can be `:stroke`, `:fill` or other symbols (check the Luxor documentation for details) (default: :stroke)
-- `close` whether the polygon should be closed or not (default: closed) 
+- `close` whether the polygon should be closed or not (default: closed)
 - `reversepath` can be set to `true` to reverse the path and create a polygon hole
 """
 JPoly(
@@ -31,3 +31,71 @@ JPoly(
         linewidth = linewidth,
         action = action,
     ) -> _JPoly(pointlist, color, linewidth, action, close, reversepath)
+
+"""
+TODO: Add documentation
+"""
+JPoly(
+    pointlist,
+    color = "white",
+    linewidth = 1,
+    action = :stroke,
+    close = true,
+    reversepath = false,
+    image_path = "",
+    scale_factor = :inset,
+) =
+    (
+        args...;
+        pointlist = pointlist,
+        color = color,
+        linewidth = linewidth,
+        action = action,
+        image_path = image_path,
+        scale_factor = scale_factor,
+    ) -> _JPoly(
+        pointlist,
+        color,
+        linewidth,
+        action,
+        close,
+        reversepath,
+        image_path,
+        scale_factor,
+    )
+
+function _JPoly(
+    pointlist,
+    color,
+    linewidth,
+    action,
+    close,
+    reversepath,
+    image_path,
+    scale_factor,
+)
+    img = readpng(image_path)
+    bbox = BoundingBox(poly(pointlist, :path))
+
+    if !isnothing(color)
+        sethue(color)
+        setline(linewidth)
+        poly(pointlist, action; close = close, reversepath = reversepath)
+    end
+
+    if scale_factor == :inset
+        boxside = max(boxwidth(bbox), boxheight(bbox))
+        imageside = max(img.width, img.height)
+    elseif scale_factor == :clip
+        boxside = min(boxwidth(bbox), boxheight(bbox))
+        imageside = min(img.width, img.height)
+    end
+    
+    poly(pointlist, :clip)
+    scalefactor = boxside / imageside
+
+    translate(boxmiddlecenter(bbox))
+    scale(scalefactor)
+    placeimage(img, centered = true)
+
+end
