2c18b27145c9c43a367b621ea1a518a68dcc0862
Jacob Zelko <jacobszelko@gmail.com>
2022-01-16 19:51:13 -0500

    Added image support dispatch for JRect

--- src/shorthands/JRect.jl
+++ src/shorthands/JRect.jl
@@ -45,3 +45,67 @@ JRect(
 
 JRect(xmin::Int64, ymin::Int64, w::Real, h::Real; kwargs...) =
     JRect(Point(xmin, ymin), w, h; kwargs...)
+
+
+"""
+TODO: Add Documentation
+"""
+JRect(
+    cornerpoint::Point,
+    w::Real,
+    h::Real,
+    color = "black",
+    linewidth = 1,
+    action = :stroke,
+    image_path = "",
+    scale_factor = :inset
+) =
+    (
+        args...;
+        cornerpoint = cornerpoint,
+        w = w,
+        h = h,
+        color = color,
+        linewidth = linewidth,
+        action = action,
+	image_path = image_path,
+	scale_factor = scale_factor
+    ) -> _JRect(cornerpoint, w, h, color, linewidth, action, image_path, scale_factor)
+
+function _JRect(
+    cornerpoint::Point,
+    w::Real,
+    h::Real,
+    color,
+    linewidth::Real,
+    action::Symbol,
+    image_path,
+    scale_factor
+)
+    bbox = BoundingBox(box(cornerpoint, w, h, :path))
+
+    if !isnothing(color)
+        sethue(color)
+        box(cornerpoint, w, h, :fill)
+    end
+
+    img = readpng(image_path)
+
+    if scale_factor == :inset
+	side = minimum([w, h])
+        bbox = BoundingBox(box(cornerpoint, side, side, action = :path))
+        boxside = max(boxwidth(bbox), boxheight(bbox))
+        imageside = max(img.width, img.height)
+        box(cornerpoint, side, side, action = :clip)
+    elseif scale_factor == :clip
+        bbox = BoundingBox(rect(O, w, h, action = :path))
+        boxside = min(boxwidth(bbox), boxheight(bbox))
+        imageside = min(img.width, img.height)
+        rect(O, w, h, action = :clip)
+    end
+
+    translate(boxmiddlecenter(bbox))
+    scale(boxside / imageside)
+    placeimage(img, centered = true)
+    return cornerpoint
+end
