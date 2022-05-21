07b6bba70ad00873b4c74fd2e7bb70c954265ead
John George Francis <arbitrandomuser@gmail.com>
2022-05-22 01:56:04 +0530

    moved JPaths out to another file

--- src/structs/Object.jl
+++ src/structs/Object.jl
@@ -1,33 +1,4 @@
-"""
-    JPath
-
-a polygon representation of a path, every Object will have a list of JPaths under the
-field jpaths.
-
-Every JPath has the following fields.
-
-- `polys` a list of polygons that makes up the path
-- `closed` a list of bools of same length as `polys`. closed[i] 
-states if polys[i] is a closed polygon or not
-- `fill` a vector of 4 numbers , R, G ,B and A the color it
-should be filled with. if the path was not filled its A is set to 0
-(along with R,G,B) this way its an "invisible" fill.
-- `stroke a vector of 4 numbers just like `fill` for the stroke color.
-- linewidth for stroke 
-"""
-mutable struct JPath
-    polys::Vector{Vector{Point}}
-    closed::Vector{Bool}
-    fill::Vector{Number}
-    stroke::Vector{Number}
-    lastaction::Symbol #last action was fill or stroke 
-    linewidth::Number
-    #TODO transform and dashstyle
-    #maybe dont have to store the transform here, just apply the transform
-    #on the polys before storing it ?. This way things like bounding
-    #boxes of the object can be computed easily at compute time.
-end
-
+include("structs/JPath.jl")
 
 """
     Object
@@ -65,7 +36,6 @@ holds the current object in an array to be declared as a constant
 The current object can be accessed using CURRENT_OBJECT[1]
 """
 const CURRENT_OBJECT = Array{Object,1}()
-
 
 const PREVIOUS_OBJECT = Array{Object,1}()
 const CURRENT_OBJECT_ACTION_TYPE = Array{Symbol,1}()
@@ -143,63 +113,6 @@ function Object(frames, func::Function, start_pos::Union{Object,Point}; kwargs..
     end
 
     return object
-end
-
-CURRENT_JPATHS = JPath[] #TODO change to const later
-CURRENT_FETCHPATH_STATE = false
-
-function getjpaths(func::Function, args = [])
-    try
-        Drawing()
-        empty!(CURRENT_JPATHS)
-        global CURRENT_FETCHPATH_STATE = true
-        v, o, f = nothing, nothing, nothing
-        #for now just make it nothing,
-        #this will cause problems if the user defines  
-        #the Object.func with types for the arguments
-        #TODO discuss a solution for this.
-        func(v, o, f, args...)
-        global CURRENT_FETCHPATH_STATE = false
-        finish()
-        ret = deepcopy(CURRENT_JPATHS)
-        empty!(CURRENT_JPATHS)
-        return ret
-    catch
-        @warn "Could not extract jpath for object,\nperhaps 
-        Object.func depends on rendertime variables"
-        return []
-    end
-end
-
-function drawobj_jpaths(obj::Object)
-    drawjpaths(obj.jpaths)
-end
-
-function drawjpaths(jpaths::Array{JPath})
-    for jpath in jpaths
-        for (polyi, co_state) in zip(jpath.polys, jpath.closed)
-            #place the polys
-            if length(polyi) > 1
-                #TODO maybe prune all single-point polys before they are added to obj.jpaths
-                poly(polyi; action = :path, close = co_state)
-            end
-        end
-        if jpath.lastaction == :stroke
-            Luxor.setcolor(jpath.fill[1:3]...)
-            Luxor.setopacity(jpath.fill[4])
-            Luxor.fillpreserve()
-            Luxor.setcolor(jpath.stroke[1:3]...)
-            Luxor.setopacity(jpath.stroke[4])
-            Luxor.strokepath()
-        else
-            Luxor.setcolor(jpath.stroke[1:3]...)
-            Luxor.setopacity(jpath.stroke[4])
-            Luxor.strokepreserve()
-            Luxor.setcolor(jpath.fill[1:3]...)
-            Luxor.setopacity(jpath.fill[4])
-            Luxor.fillpath()
-        end
-    end
 end
 
 """
