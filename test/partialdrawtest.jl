9fd1bac7b236a629241c5d580bc32103f75e5382
John George Francis <arbitrandomuser@gmail.com>
2022-06-28 18:09:10 +0530

    added 2 more images for refs, added the ref images ,format

--- test/partialdrawtest.jl
+++ test/partialdrawtest.jl
@@ -1,11 +1,11 @@
 function draw_complex_shape()
-    circle(O+20,100,:stroke)
+    circle(O + 20, 100, :stroke)
     sethue("red")
-    box(O,100,120,:fillpreserve)
+    box(O, 100, 120, :fillpreserve)
     sethue("black")
     strokepath()
     sethue("green")
-    ngon(Point(-200,-200),50;action=:fillstroke)
+    ngon(Point(-200, -200), 50; action = :fillstroke)
 end
 
 
@@ -13,11 +13,13 @@ end
     video = Video(500, 500)
     back = Background(1:20, (args...) -> background("white"))
     obj = Object(1:20, (args...) -> draw_complex_shape())
-    act!(obj, Action(2:20, showcreation() ))
+    act!(obj, Action(2:20, showcreation()))
     render(video; tempdirectory = "images", pathname = "")
 
-    @test_reference "refs/star2circle5.png" load("images/0000000005.png")
-    @test_reference "refs/star2circle15.png" load("images/0000000015.png")
+    @test_reference "refs/partialdraw5.png" load("images/0000000005.png")
+    @test_reference "refs/partialdraw10.png" load("images/0000000010.png")
+    @test_reference "refs/partialdraw15.png" load("images/0000000015.png")
+    @test_reference "refs/partialdraw18.png" load("images/0000000018.png")
 
     for i in 1:20
         rm("images/$(lpad(i, 10, "0")).png")
