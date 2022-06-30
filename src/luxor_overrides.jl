6f9a4d5f5998f5d7f9a7bea78ccccab942c4e747
John George Francis <arbitrandomuser@gmail.com>
2022-06-30 21:08:20 +0530

    moved overrides to Luxor.jl iteslf

--- src/luxor_overrides.jl
+++ src/luxor_overrides.jl
@@ -1,4 +1,6 @@
 DISABLE_LUXOR_DRAW = false
+CURRENT_FETCHPATH_STATE=false
+
 """
     setline(linewidth)
 
@@ -298,130 +300,6 @@ function apply_transform(transform::Vector{Float64}, poly::Vector{Point})
     end
     retpoly
 end
-
-"""
-    update_currentjpath(action::Symbol)
-
-Updates the CURRENT_JPATHS
-This function is used  inside the strokepath/strokepreserve/fillpath/fillpreserve.
-Converts the current Path and other drawing states into a JPath and appends to the CURRENT_JPATHS global.
-
-the argument is a symbol either `:stroke` or `:fill` , to change behaviour
-for stroke vs fill.
-"""
-function update_currentjpath(action::Symbol)
-    #println("test strokepaths")
-    cur_polys, cur_costates = pathtopoly(Val(:costate))
-    @assert length(cur_polys) == length(cur_costates)
-    transform = getmatrix()
-    cur_polys = [apply_transform(transform, poly) for poly in cur_polys]
-    #cur_polys is of 2 element Tuple 
-    #containg 2 arrays 1 with Polygons and one with the bools
-    r, g, b, a = map(
-        sym -> getfield(Luxor.CURRENTDRAWING[1], sym),
-        [:redvalue, :greenvalue, :bluevalue, :alpha],
-    )
-    #by default they are black transparent 
-    fillstroke = Dict(:fill => [0.0, 0, 0, 0], :stroke => [0.0, 0, 0, 0])
-    fillstroke[action] .= [r, g, b, a]
-    #if polys didnt change we just modify the last JPath in the CURRENT_JPATHS
-    if length(CURRENT_JPATHS) > 0 && cur_polys == CURRENT_JPATHS[end].polys
-        #print("found similar path\n")
-        setproperty!(CURRENT_JPATHS[end], action, fillstroke[action])
-        #if action was :stroke above line would be the same as doing...
-        #CURRENT_JPATHS[end].stroke = fillstroke[:stroke] 
-        setproperty!(CURRENT_JPATHS[end], :lastaction, action)
-        #if action was :stroke above line would be the same as doing...
-        #CURRENT_JPATHS[end].lastaction = :stroke
-    else
-        #println("adding to CURRENT_JPATH")
-        currpath = JPath(
-            cur_polys,
-            cur_costates,
-            fillstroke[:fill],
-            fillstroke[:stroke],
-            action,
-            2,
-        )
-        push!(CURRENT_JPATHS, currpath)
-    end
-end
-
-#The following four functions over-ride luxors strokepath/strokepreserve/fillpath/fillpreserve function.
-
-#Behaviour depends on 2 global variables.
-#if CURRENT_FETCHPATH_STATE is true -> the current path is converted
-#                                      to a JPath and pushed into CURRENT_JPATHS.
-#
-#if DISABLE_LUXOR_DRAW is true -> does not draw on the canvas , clear the path with `newpath()` if not a "preserve" function.
-#else -> do exactly what original Luxor.strokepath/fillpath etc would do i.e place the path on the canvas and stroke/fill it.
-"""
-    strokepath()
-
-Stroke the current path with the current line width, line join, line cap, dash, and stroke
-scaling settings. The current path is then cleared.
-"""
-function Luxor.strokepath()
-    if CURRENT_FETCHPATH_STATE
-        update_currentjpath(:stroke)
-    end
-    if !DISABLE_LUXOR_DRAW
-        Luxor.get_current_strokescale() ?
-        Luxor.Cairo.stroke_transformed(Luxor.get_current_cr()) :
-        Luxor.Cairo.stroke(Luxor.get_current_cr())
-    else
-        newpath()
-    end
-end
-
-"""
-    strokepreserve()
-
-Stroke the current path with current line width, line join, line cap, dash, and stroke
-scaling settings, but then keep the path current.
-"""
-function Luxor.strokepreserve()
-    if CURRENT_FETCHPATH_STATE
-        update_currentjpath(:stroke)
-    end
-    if !DISABLE_LUXOR_DRAW
-        Luxor.get_current_strokescale() ?
-        Luxor.Cairo.stroke_preserve_transformed(Luxor.get_current_cr()) :
-        Luxor.Cairo.stroke_preserve(Luxor.get_current_cr())
-    end
-
-end
-
-"""
-    fillpath()
-
-Fill the current path according to the current settings. The current path is then cleared.
-"""
-function Luxor.fillpath()
-    if CURRENT_FETCHPATH_STATE
-        update_currentjpath(:fill)
-    end
-    if !DISABLE_LUXOR_DRAW
-        Luxor.Cairo.fill(Luxor.get_current_cr())
-    else
-        newpath()
-    end
-end
-
-"""
-    fillpreserve()
-
-Fill the current path with current settings, but then keep the path current.
-"""
-function Luxor.fillpreserve()
-    if CURRENT_FETCHPATH_STATE
-        update_currentjpath(:fill)
-    end
-    if !DISABLE_LUXOR_DRAW
-        Luxor.Cairo.fill_preserve(Luxor.get_current_cr())
-    end
-end
-
 
 """
    pathtopoly(::Val{:costate})
