409be68d5d52bd8691912835a0be0fd313174ea0
John George Francis <arbitrandomuser@gmail.com>
2022-07-22 22:46:14 +0530

    added test for keyframed morphs and refs

--- test/morphing.jl
+++ test/morphing.jl
@@ -1,7 +1,3 @@
-function redstarfunc()
-    sethue("red")
-    star(O, 30, 5, 0.5, 0, :stroke)
-end
 #circfunc() = circle(Point(-100,-100),30,5,0.5,0,:stroke)
 function bluecircfunc()
     sethue("blue")
@@ -10,14 +6,23 @@ function bluecircfunc()
     strokepath()
 end
 
+function starfunc(color)
+    sethue(color)
+    star(O,30,5,0.5,0,:stroke)
+end
+
+function boxfunc(color)
+    sethue(color)
+    box(O-50,50,50,:stroke)
+end
 
 @testset "Morphing star to circle and back , morph to function" begin
     video = Video(500, 500)
     back = Background(1:20, (args...) -> background("white"))
-    star_obj = Object(1:20, (args...) -> redstarfunc(), Point(-100, -100))
+    star_obj = Object(1:20, (args...) -> starfunc("red"), Point(-100, -100))
     act!(star_obj, Action(2:10, morph_to(bluecircfunc)))
     act!(star_obj, Action(2:10, anim_translate(Point(200, 200))))
-    act!(star_obj, Action(11:20, morph_to(redstarfunc)))
+    act!(star_obj, Action(11:20, morph_to(starfunc,["red"])))
     act!(star_obj, Action(11:20, anim_translate(Point(-200, -200))))
     render(video; tempdirectory = "images", pathname = "")
 
@@ -33,7 +38,7 @@ end
     video = Video(500, 500)
     back = Background(1:20, (args...) -> background("white"))
     circ_obj = Object(1:20, (args...) -> bluecircfunc())
-    star_obj = Object(1:20, (args...) -> redstarfunc())
+    star_obj = Object(1:20, (args...) -> starfunc("red"))
     act!(star_obj, Action(2:20, morph_to(circ_obj)))
 
     render(video; tempdirectory = "images", pathname = "")
@@ -44,7 +49,20 @@ end
     for i in 1:20
         rm("images/$(lpad(i, 10, "0")).png")
     end
+end
 
+@testset "Morphing using keyframes" begin
+    video = Video(500,500)
+    back = Background(1:20,(args...) -> background("white"))
+    circ_obj = Object(1:20, (args...) -> bluecircfunc())
+    act!(circ_obj, Action(2:20, Animation([0,0.3,1],MorphFunction[bluecircfunc,(boxfunc,["red"]),(starfunc,"blue")] ),morph()))
+    render(video; tempdirectory = "images", pathname = "")
+    @test_reference "refs/star2circle_keyframe5.png" load("images/0000000005.png")
+    @test_reference "refs/star2circle_keyframe15.png" load("images/0000000015.png")
+
+    for i in 1:20
+        rm("images/$(lpad(i, 10, "0")).png")
+    end
 end
 
 @testset "Morphing mutates the object" begin
@@ -63,9 +81,9 @@ end
 
     video = Video(500, 500)
     back = Background(1:200, ground)
-    star_obj = Object(1:200, (args...) -> greenbox())
-    act!(star_obj, Action(10:20, morph_to(bluecircfunc)))
-    act!(star_obj, Action(30:40, anim_translate(Point(100, -100))))
+    start_obj = Object(1:200, (args...) -> greenbox())
+    act!(start_obj, Action(10:20, morph_to(bluecircfunc)))
+    act!(start_obj, Action(30:40, anim_translate(Point(100, -100))))
 
     #TODO: the following comment is from the old test.
     # ask Ole if new morph behaviour is working right.
@@ -74,11 +92,11 @@ end
     # the the origin is shifted and neve restored back
     # you can see this in the result of anim_translate actions
     # TODO: fix this at some point!
-    act!(star_obj, Action(40:60, morph_to(redstarfunc)))
-    act!(star_obj, Action(70:90, anim_translate(Point(100, -100))))
-    act!(star_obj, Action(100:120, morph_to(greenbox)))
-    act!(star_obj, Action(130:150, anim_translate(Point(-100, 50))))
-    act!(star_obj, Action(160:180, morph_to(bluecircfunc)))
+    act!(start_obj, Action(40:60, morph_to(starfunc,["red"])))
+    act!(start_obj, Action(70:90, anim_translate(Point(100, -100))))
+    act!(start_obj, Action(100:120, morph_to(greenbox)))
+    act!(start_obj, Action(130:150, anim_translate(Point(-100, 50))))
+    act!(start_obj, Action(160:180, morph_to(bluecircfunc)))
     render(video; tempdirectory = "images", pathname = "")
 
     for i in [
